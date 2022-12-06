b0fbb42c23e64fc253af57f20fe9553a23a4c182
Zhiwei Shao <magicianshao@outlook.com>
2022-12-06 21:26:35 +0800

    Revert "temp adjustments to enable multiprocessing."
    This reverts commit 40b68031a5a4994dd6ab6f96c58fd28afb38fcce.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 
-setup(name = 'XIGrMtest',
-      version = '0.2',
+setup(name = 'XIGrM',
+      version = '0.1',
       description = 'Analysis tools for the intragroup medium.',
       url = 'https://github.com/Hoptunes/XIGrM',
       author = 'Zhiwei Shao',
-      author_email = 'zwshao@sjtu.edu.cn',
+      author_email = 'zws@smail.nju.edu.cn',
       license = '',
       packages = find_packages(),
       zip_safe = False,
