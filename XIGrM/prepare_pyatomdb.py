05450f7d2336fa59f6c48b63b985a017775c305b
Zhiwei Shao <magicianshao@outlook.com>
2024-01-29 19:31:31 +0800

    fix indentation

--- XIGrM/prepare_pyatomdb.py
+++ XIGrM/prepare_pyatomdb.py
@@ -113,10 +113,10 @@ def calculate_continuum_emission(energy_bins, specific_elements = atomicNumbers,
     if return_spectra:
         cont_spectra = np.zeros((len(atomic) + 1, len(cut), len(energy_bins)-1))
 		
-	cie = pyatomdb.spectrum.CIESession(linefile = line_file, cocofile = coco_file)
-	cie.set_response(energy_bins, raw=True)
-	cie.set_abund(atomic_in_atomdb, 0.)
-	cie.set_eebrems(False)
+    cie = pyatomdb.spectrum.CIESession(linefile = line_file, cocofile = coco_file)
+    cie.set_response(energy_bins, raw=True)
+    cie.set_abund(atomic_in_atomdb, 0.)
+    cie.set_eebrems(False)
 
     for i in range(0, len(cut)):
         print('Cont. Temperature: %g K' % cut[i])
